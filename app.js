bcc02427916126cf35b535ca3fa341060b81e719
Will Wilkinson <wfw40869@gmail.com>
2020-09-24 23:10:19 -0500

    Removed DB call from app since we don't need to persist forecasts

--- app.js
+++ app.js
@@ -1,33 +1,15 @@
 require('dotenv').config();
 
-var express = require('express'),
-    app     = express(),
-    bodyParser  = require("body-parser"),
+let express = require('express'),
+    app = express(),
+    bodyParser = require("body-parser"),
     serveStatic = require('serve-static'),
-    moment      = require("moment"),
-    mongoose = require('mongoose'),
+    moment = require('moment'),
     flash = require("connect-flash"),
     request = require('request');
 
-//SCHEMA SETUP
-
-var fiveDaySchema = mongoose.Schema({
-   city: {},
-   coord: {},
-   country: String,
-   cod: Number,
-   message: Number,
-   cnt: Number,
-   list: [{}]
-});
-
-var FiveDayForecast = mongoose.model("FiveDayForecast", fiveDaySchema);
-
-var url = process.env.DATABASEURL || "mongodb+srv://" + process.env.USERNAME + ":" + process.env.PASSWORD + "@weather-app-project.vmoyg.mongodb.net/weather_app_project?retryWrites=true&w=majority";
-mongoose.connect(url, { useNewUrlParser: true });
-
 app.use(flash());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(serveStatic("public/"));
 
@@ -38,7 +20,7 @@ app.use(require("express-session")({
     saveUninitialized: false
 }));
 
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.locals.moment = require("moment");
     res.locals.error = req.flash("error");
     next();
@@ -47,14 +29,14 @@ app.use(function(req, res, next){
 
 
 //INDEX ROUTE
-app.get("/", function(req, res){
-   // Get rid of the DB part of this app...
-   //clearDB();
+app.get("/", function (req, res) {
+    // Get rid of the DB part of this app...
+    //clearDB();
     res.render("landing");
 });
 
 // CREATE - add new forecast to DB
-app.post("/weather", function(req, res) {
+app.post("/weather", function (req, res) {
     // get zip from user
     let zip = req.body.zip;
     let apiKey = process.env.WEATHER_API_KEY;
@@ -62,27 +44,20 @@ app.post("/weather", function(req, res) {
 
     if (checkZip(zip)) {
         //make request to API for info
-        request(fiveDayURL, function(err, response, body) {
-            //store forecast info in DB
-            var newFiveDayForecast = JSON.parse(body);
+        request(fiveDayURL, function (err, response, body) {
+            let newFiveDayForecast = JSON.parse(body);
             if (err) {
-                req.flash("error", "Please enter a valid zip code!");
+                req.flash("error", "There was an issue fetching forecasts");
                 res.redirect("/");
             }
             else {
-                FiveDayForecast.create(newFiveDayForecast, function(err, newlyCreated) {
-                    var minsAndMaxes;
-                    if (err) {
-                        req.flash("error", "There was an issue getting your weather forecasts");
-                        res.redirect("/");
-                    } else if(newFiveDayForecast.message === "city not found") {
-                        req.flash("error", "City was not found. Please try again.");
-                        res.redirect("/");
-                    }
-                    else {
-                        res.render("home", { fiveDay: newFiveDayForecast, minsAndMaxes: findMinMax(newFiveDayForecast) });
-                    }
-                })
+                if (newFiveDayForecast.message === "city not found") {
+                    req.flash("error", "City was not found. Please try again.");
+                    res.redirect("/");
+                }
+                else {
+                    res.render("home", { fiveDay: newFiveDayForecast, minsAndMaxes: findMinMax(newFiveDayForecast) });
+                }
             }
         });
     }
@@ -92,66 +67,48 @@ app.post("/weather", function(req, res) {
     }
 });
 
-
-
-function clearDB() {
-    mongoose.connect(url, { useNewUrlParser: true }, function(err, db) {
-        if (err) {
-            console.log(err);
-        }
-        else {
-           console.log('db', db)
-            db.collection('fivedayforecasts').deleteMany(function(err, result) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-        }
-    });
-}
-
 function checkZip(zip) {
-    return(/^[0-9]{5}(?:-[0-9]{4})?$/.test(zip));
+    return (/^[0-9]{5}(?:-[0-9]{4})?$/.test(zip));
 }
 
 //Find true min and max temperatures for each day
-function findMinMax(fiveDayForecast){
-    var minsAndMaxes = [];
-    var min = 9999;
-    var max = 0;
-    var day = parseInt(fiveDayForecast.list[0].dt_txt.slice(8,10));
-    var count = 0;
+function findMinMax(fiveDayForecast) {
+    let minsAndMaxes = [];
+    let min = 9999;
+    let max = 0;
+    let day = parseInt(fiveDayForecast.list[0].dt_txt.slice(8, 10));
+    let count = 0;
     //loop through fiveDayForecasts.list
-    while(count < fiveDayForecast.list.length){
+    while (count < fiveDayForecast.list.length) {
         //Check if hour reaches last entry which is 21 OR it's a new day AND max temp is not equal to 0
-        if((parseInt(fiveDayForecast.list[count].dt_txt.slice(11,13)) === 21 || day !== parseInt(fiveDayForecast.list[count].dt_txt.slice(8,10))) && max !== 0) {
+        if ((parseInt(fiveDayForecast.list[count].dt_txt.slice(11, 13)) === 21 || day !== parseInt(fiveDayForecast.list[count].dt_txt.slice(8, 10))) && max !== 0) {
             //check the true min and max on last time
-            if(min > fiveDayForecast.list[count].main.temp_min){
+            if (min > fiveDayForecast.list[count].main.temp_min) {
                 min = fiveDayForecast.list[count].main.temp_min;
             }
-            else if(max < fiveDayForecast.list[count].main.temp_max){
+            else if (max < fiveDayForecast.list[count].main.temp_max) {
                 max = fiveDayForecast.list[count].main.temp_max;
             }
             //Then add that new information to the end of the minsAndMaxes array
-            minsAndMaxes.push({ date: fiveDayForecast.list[count].dt_txt, min_temp: min, max_temp: max});
+            minsAndMaxes.push({ date: fiveDayForecast.list[count].dt_txt, min_temp: min, max_temp: max });
             //update the new day, and reset min/max
-            day = parseInt(fiveDayForecast.list[count].dt_txt.slice(8,10));
+            day = parseInt(fiveDayForecast.list[count].dt_txt.slice(8, 10));
             min = 9999;
             max = 0;
         }
-        else if(min > fiveDayForecast.list[count].main.temp_min){
-             min = fiveDayForecast.list[count].main.temp_min;
+        else if (min > fiveDayForecast.list[count].main.temp_min) {
+            min = fiveDayForecast.list[count].main.temp_min;
         }
-        else if(max < fiveDayForecast.list[count].main.temp_max){
+        else if (max < fiveDayForecast.list[count].main.temp_max) {
             max = fiveDayForecast.list[count].main.temp_max;
         }
         count++;
     }
-    return(minsAndMaxes);
+    return (minsAndMaxes);
 }
 
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The Server Has Started!") ;
+app.listen(process.env.PORT, process.env.IP, function () {
+    console.log("The Server Has Started!");
 });
